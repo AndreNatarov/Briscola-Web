98f802dde424e668e9016b34355ac269d916f76b
AndreNatarov <andrenatarovmauri@gmail.com>
2023-10-11 16:10:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+class Carta{
+
+    constructor(valore,valore_punteggio,seme,briscola){
+
+        this.valore = valore;
+        this.valore_punteggio = valore_punteggio;
+        this.seme = seme["spada","denaro","bastone","coppa"];
+        this.briscola = briscola;
+
+    }
+
+}
